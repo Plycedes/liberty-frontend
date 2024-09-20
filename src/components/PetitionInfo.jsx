fe57912363261fde2e3cc399305b8ce958e2187d
Plycedes <abhaypratapyadav340@gmail.com>
2024-09-20 13:14:08 +0530

    PetitionInfo

--- src/components/PetitionInfo.jsx
+++ src/components/PetitionInfo.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
+import Jazzicon, { jsNumberForAddress } from "react-jazzicon";
 
 function PetitionInfo() {
     const [petition, setPetition] = useState({});
@@ -15,17 +16,39 @@ function PetitionInfo() {
     return (
         <div className="text-white">
             {petition ? (
-                <div className="">
-                    <h1>{petition.owner}</h1>
-                    <h1>{petition.title}</h1>
-                    <h1>{petition.description}</h1>
-                    <h1>{petition.votes}</h1>
-                    <button
-                        className="text-white font-semibold bg-purple-600 rounded-lg pb-2 pl-2 pr-2 pt-2"
-                        onClick={() => navigate(-1)}
-                    >
-                        {`<- Back`}
-                    </button>
+                <div className="flex w-full h-screen items-center">
+                    <div className="w-1/2 flex justify-center border">
+                        <div className="w-full m-3 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                            <img
+                                className="rounded-t-lg w-full h-[320px]"
+                                src={petition.image}
+                                alt="Campaign Image"
+                            />
+                            <div className="m-3">
+                                <h1 className="text-2xl text-purple-500 font-bold my-2">
+                                    {petition.title}
+                                </h1>
+                                <h1 className="text-gray-300 my-2">{petition.description}</h1>
+                                <h1>{petition.votes}</h1>
+
+                                <div className="flex my-3 gap-2 items-center">
+                                    <Jazzicon
+                                        diameter={30}
+                                        seed={jsNumberForAddress(String(petition.owner))}
+                                    />
+                                    <h1>{petition.owner}</h1>
+                                </div>
+
+                                <button
+                                    className="text-white font-semibold bg-purple-600 rounded-lg pb-2 pl-2 pr-2 pt-2"
+                                    onClick={() => navigate(-1)}
+                                >
+                                    {`<- Back`}
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="w-1/2 flex justify-center border">Voters</div>
                 </div>
             ) : (
                 <button
